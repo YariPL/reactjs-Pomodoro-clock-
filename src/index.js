78dbc3a061c906c65548f08c17646bb955a91f4a
Yari PL <yaroslav.khudchenko@gmail.com>
2019-02-23 20:57:07 +0100

    connect countdown timer to script

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Countdown from 'react-countdown-now';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
